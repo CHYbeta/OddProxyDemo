66963b1fc75d0f9e3eb564e7667babc936bb31ea
chybeta <chybet4@gmail.com>
2021-09-23 17:39:40 +0800

    update

--- nginx/demo1/backend/gunicorn_demo.py
+++ nginx/demo1/backend/gunicorn_demo.py
@@ -1,13 +1,19 @@
 from flask import Flask
+import logging
+
 
 app = Flask(__name__)
-
+gunicorn_error_logger = logging.getLogger('gunicorn.error')
+app.logger.handlers.extend(gunicorn_error_logger.handlers)
+app.logger.setLevel(logging.DEBUG)
 
 @app.route('/public', methods=['GET'])
 def public():
+    app.logger.debug('hit public')
     return "gunicorn public endpoint"
 
 
 @app.route('/private', methods=['GET'])
 def private():
+    app.logger.debug('hit private')
     return "gunicorn private endpoint"
